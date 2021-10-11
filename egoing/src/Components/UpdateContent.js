a522091504583f63d7936315056c968c4490f383
m-veloper <m_veloper@naver.com>
2021-10-12 01:59:55 +0900

    no message

--- egoing/src/Components/UpdateContent.js
+++ egoing/src/Components/UpdateContent.js
@@ -3,12 +3,7 @@ import React, {Component} from "react";
 class ReadContent extends Component {
   render() {
     return (
-      <article>
-        <h2>Create</h2>
-        <form>
-          <p><input type={"text"} name={"title"} placeholder={"title"}/></p>
-        </form>
-      </article>
+      <a>update</a>
     );
   }
 }
