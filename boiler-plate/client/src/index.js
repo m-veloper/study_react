b95520fcdc7db3ac41cc6f31f082f9c6dd99378f
m-veloper <m_veloper@naver.com>
2021-10-16 02:03:04 +0900

    concurrently 적용, antd css 적용, redux 적용

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "react-redux";
+import Reducer from "./_reducer"
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={createStoreWithMiddleware(Reducer,
+    window.__REDUX_DEVTOOLS_EXTENTSION__ &&
+                window.__REDUX_DEVTOOLS_EXTENTSION__()
+  )}>
+    <App/>
+  </Provider>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
