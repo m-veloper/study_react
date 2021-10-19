1b0dc1b51e57ef0696dbdcbfb0a698d09087a23e
m-veloper <m_veloper@naver.com>
2021-10-20 03:27:43 +0900

    사이드 비디오 리스트

--- youtub-clone/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ youtub-clone/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {List, Avatar, Row, Col} from 'antd';
 import axios from 'axios';
-// import SideVideo from './Sections/SideVideo';
+import SideVideo from './Sections/SideVideo';
 // import Subscriber from './Sections/Subscriber';
 // import Comments from './Sections/Comments'
 // import LikeDislikes from './Sections/LikeDislikes';
@@ -69,7 +69,7 @@ function VideoDetailPage(props) {
           </div>
         </Col>
         <Col lg={6} xs={24}>
-          {/*<SideVideo/>*/}
+          <SideVideo/>
         </Col>
       </Row>
     )
