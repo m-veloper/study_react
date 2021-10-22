9e5acdb80dc68dad8aa54ebc20140852263874e9
m-veloper <m_veloper@naver.com>
2021-10-23 02:10:36 +0900

    랜딩페이지

--- movie/client/src/components/Config.js
+++ movie/client/src/components/Config.js
@@ -2,3 +2,5 @@
 export const USER_SERVER = '/api/users';
 export const API_URL = "https://api.themoviedb.org/3/";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+export const API_KEY = "cbae508ce593e8cccb3d55f9afcca9cc";
+
