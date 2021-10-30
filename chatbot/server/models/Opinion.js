2e54bfe017233293d0287076bb639610031dee83
m-veloper <m_veloper@naver.com>
2021-10-30 16:52:14 +0900

    no message

--- chatbot/server/models/Opinion.js
+++ chatbot/server/models/Opinion.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const opinionSchema =  mongoose.Schema({
-    name: String,
-    opinion: String,
-    email: String,
-    registerDate: Date
+const opinionSchema = mongoose.Schema({
+  name: String,
+  opinion: String,
+  email: String,
+  registerDate: Date
 });
 
 const Opinion = mongoose.model('Opinion', opinionSchema);
 
-module.exports = { Opinion }
+module.exports = {Opinion}
