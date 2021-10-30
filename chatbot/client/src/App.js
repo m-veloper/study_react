84fc1a91483a229921647ef3fca2262381a200ce
m-veloper <m_veloper@naver.com>
2021-10-30 16:51:32 +0900

    no message

--- chatbot/client/src/App.js
+++ chatbot/client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Typography, Icon } from 'antd';
-const { Title } = Typography;
+import {Typography, Icon} from 'antd';
+
+const {Title} = Typography;
 
 function App() {
   return (
     <div>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
-        <Title level={2} >CHAT BOT APP&nbsp;<Icon type="robot" /></Title>
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '2rem'}}>
+        <Title level={2}>CHAT BOT APP&nbsp;<Icon type="robot"/></Title>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-       
+      <div style={{display: 'flex', justifyContent: 'center'}}>
       </div>
     </div>
   )
